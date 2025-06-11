41227e4afdcf4bcaed2bf4ad04bec32b030e4f69
BrijeshVaghasiya747 <brijesh.v.growcode@gmail.com>
2025-06-11 11:28:50 +0530

    2-bharat-clock

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import ClockHeading from './commponents/clockHeading';
+import ClockSlogan from './commponents/ClockSlogan';
+import CurrentTime from './commponents/CurrentTime';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css";
+
+
+import './App.css'
+
+
+function App() {
+  return (
+  <center>
+    <ClockHeading />
+    <ClockSlogan />
+    <CurrentTime />
+  
+    </center> 
+  );
+}
+
+export default App
